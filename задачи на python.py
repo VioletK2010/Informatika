3fad336581687d69274755e4f95f34ed1d54567a
VioletK2010 <123331023+VioletK2010@users.noreply.github.com>
2023-01-22 22:54:55 +0200

    Informatika

---
+++ задачи на python.py
@@ -0,0 +1,29 @@
+Python 3.10.9 (tags/v3.10.9:1dd9be6, Dec  6 2022, 20:01:21) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # черепашка
+>>> 
+>>> from turtle import*
+>>> Pen()
+<turtle.Turtle object at 0x000001AF5AC50130>
+>>> shape('turtle')
+>>> fd(100)
+>>> right(90)
+>>> color("red")
+>>> fd(100)
+>>> right(90)
+>>> color("green")
+>>> fd(100)
+>>> right(90)
+>>> color("blue")
+>>> fd(100)
+>>> right(90)
+>>> 
+>>> # задача 1
+>>> 
+>>> x = 3
+>>> y = 4
+>>> a = 2*x + y
+>>> b = 2*x - y
+>>> c = a*b
+>>> print("c = ", c)
+c =  20
